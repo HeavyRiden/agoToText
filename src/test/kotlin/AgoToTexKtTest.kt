dfa5f82b1d4134dfc3aa878c5e5cc38c7f9bfb89
Kirill Novichkov <sufirhavat@gmail.com>
2025-01-17 10:07:10 +0700

    gradle test problems

---
+++ src/test/kotlin/AgoToTexKtTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+ class AgoToTexKtTest {
+
+@Test
+ fun wordEndingMinute() {
+  val result = wordEndingMinute(60)
+
+ assertEquals("уту", result)
+ }
+}
